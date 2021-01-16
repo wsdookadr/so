c7ba282c7e77c9439cbcb19ceb44de21b9dfe4b9
Stefan Petrea <stefan@garage-coding.com>
2021-01-16 07:47:41 +0200

    added log-scale plot in addition to regular plot

--- so-65707040/plot.sh
+++ so-65707040/plot.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-DATA="$1"
-OUTPUT="$2"
-gnuplot -e "filename='$DATA'; set terminal pngcairo size 800,600 enhanced font 'Segoe UI,10' ; set output '$OUTPUT' ; " -p plot.gnuplot
+PROGRAM="$1"
+DATA="$2"
+OUTPUT="$3"
+gnuplot -e "filename='$DATA'; set terminal pngcairo size 800,600 enhanced font 'Segoe UI,10' ; set output '$OUTPUT' ; " -p $PROGRAM
