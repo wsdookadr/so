3b215781ff211a0b4588617eb1f4ac0944d0a708
Stefan Petrea <stefan@garage-coding.com>
2021-01-17 02:39:05 +0200

    fixed a few problems with benchmark wrapper and makefile

--- so-65707040/bench.sh
+++ so-65707040/bench.sh
@@ -3,6 +3,8 @@
 PROGRAM="$1"
 START="$2"
 END="$3"
+
+echo "N,time,memory"
 for i in $(seq $START $END); do
     >&2 echo "Running for input $i"
     a=$(/usr/bin/time -f %e,%M python3 $PROGRAM $i 2>&1 >/dev/null)
