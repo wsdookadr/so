9460128fda4e725e4c5877321f02c0fd312818b5
Stefan Petrea <stefan@garage-coding.com>
2021-04-16 14:01:18 +0300

    update

---
+++ so-67118077/count2.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Lingua::EN::Numbers qw/num2en/;
+
+
+
+my $part = 0;
+my @cnt_part;
+while(my $line = <STDIN>) {
+    if($. == 1) {
+        next;
+    }elsif($line =~ m{^\(PART (\d+)\)}) {
+        $part = $1;
+    }else {
+        my @cols = split(m{\s+},$line);
+        if(@cols == 6) {
+            my $potential = $cols[3];
+            if(0.7 <= $potential) {
+                $cnt_part[$part]++;
+            };
+        };
+    };
+};
+
+for(my $i=1;$i<=$#cnt_part;$i++){
+    my $num_words = num2en($cnt_part[$i]);
+    print "Part $i:\n";
+    print "$cnt_part[$i] ($num_words values 0.7 <=)\n";
+};
+
