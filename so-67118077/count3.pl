3d185961937e2378b7770a4e0cfbc3bca915e549
Stefan Petrea <stefan@garage-coding.com>
2021-04-17 03:06:45 +0300

    update

---
+++ so-67118077/count3.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Data::Dumper;
+
+my $cnt = {};
+while(my $line = <STDIN>) {
+    if($. == 1) {
+        next;
+    }else {
+        my @cols = split(m{\s+},$line);
+        if(@cols == 6) {
+            my $potential = $cols[3];
+            my $id = $cols[0];
+            $id =~ s{^\;}{};
+            if(0.7 <= $potential) {
+                $cnt->{$id}++;
+            };
+        };
+    };
+};
+
+my @ids_found = sort { $a cmp $b } (keys %$cnt);
+
+for my $id (@ids_found) {
+    print "PART $id:\n";
+    print "$cnt->{$id} (values 0.7 <=)\n";
+};
+
